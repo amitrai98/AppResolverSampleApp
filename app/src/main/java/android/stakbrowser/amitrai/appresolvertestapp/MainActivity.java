e798483c679c8488b70135c4ce9e29a7937e7502
amit rai <bipulamit@gmail.com>
2016-01-06 16:58:47 +0530

    Initial commit

--- app/src/main/java/android/stakbrowser/amitrai/appresolvertestapp/MainActivity.java
+++ app/src/main/java/android/stakbrowser/amitrai/appresolvertestapp/MainActivity.java
@@ -6,11 +6,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView list_options = null;
+
+    final ArrayList<String> list = new ArrayList<String>();
+
+    private ArrayAdapter<String> adapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        init();
 
-        AppResolver.openYouTube(this, "https://www.youtube.com/user/tseries");
+//        AppResolver.openYouTube(this, "https://www.youtube.com/user/tseries");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +63,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * initalizing view elements
+     */
+    private void init(){
+        list_options = (ListView) findViewById(R.id.list_options);
+
+        list.add("Facebook");
+        list.add("Twitter");
+        list.add("Linkedin");
+        list.add("Youtube");
+        list.add("Gplus");
+        list.add("Bloger");
+        list.add("Pinterest");
+        list.add("Instagram");
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+
+        list_options.setAdapter(adapter);
+
+        list_options.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        AppResolver.resolve(MainActivity.this, "https://www.facebook.com/adamxadam");
+                        break;
+                    case 1:
+                        AppResolver.resolve(MainActivity.this, "https://twitter.com/narendramodi");
+                        break;
+                    case 2:
+                        AppResolver.resolve(MainActivity.this, "https://www.linkedin.com/company/infosys");
+                        break;
+                    case 3:
+                        AppResolver.resolve(MainActivity.this, "https://www.youtube.com/user/TEDtalksDirector");
+                        break;
+                    case 4:
+                        AppResolver.resolve(MainActivity.this, "https://plus.google.com/me");
+                        break;
+                    case 5:
+                        AppResolver.resolve(MainActivity.this, "android-developers.blogspot.com/");
+                        break;
+                    case 6:
+                        AppResolver.resolve(MainActivity.this, "https://www.pinterest.com/tednews/");
+                        break;
+                    case 7:
+                        AppResolver.resolve(MainActivity.this, "https://www.instagram.com/ted/");
+                        break;
+
+
+                }
+
+            }
+        });
     }
 }
